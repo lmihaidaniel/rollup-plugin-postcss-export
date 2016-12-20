ee9aa0902ce65e749fd4cfa56f18f862413b4f61
Mihai Lacatusu <lacatusu.mihai.daniel@gmail.com>
2016-12-20 12:11:28 +0100

    improved performance
     - Removed unnecessary read style from file
     - added support for in memory css
     - added support for rollup cache

--- src/index.js
+++ src/index.js
@@ -11,70 +11,86 @@ let _logSuccess = function(msg, title) {
   console.log('[' + time + ']', title || 'POSTCSS', "'" + '\x1b[32m' + msg + '\x1b[0m' + "'");
 };
 
-let noOp = function(){};
-let bucket = [];
+let noOp = function() {};
 
-function _postcss(plugins, output, onDone) {
+const styles = {};
+let changes = 0;
+
+function _postcss(styles, plugins, output, onDone) {
   _logSuccess('init');
   let r = "";
   let index = 0;
-  let n = bucket.length;
-  if(n===0) {
-    onDone();
-    return;
-  }
-  for (var k in bucket) {
-    let cb = function(f) {
-      return function(err, data) {
-        if (!err) {
-          postcss(plugins)
-            .process(data, {
-              from: f,
-              to: f
-            })
-            .then(function(result) {
-              index += 1;
-              r += result.css;
-              if (index === n) {
-                fs.writeFile(output, r, function(err) {
-                  if (err) {
-                    return console.log(err);
-                  }
-                  _logSuccess('done');
-                  onDone();
-                });
-                bucket = [];
-              }
-            });
+  let n = Object.keys(styles).length;
+  for (var file in styles) {
+    postcss(plugins)
+      .process(styles[file] || '', {
+        from: file,
+        to: file
+      })
+      .then(function(result) {
+        index += 1;
+        r += result.css;
+        if (index === n) {
+          fs.writeFile(output, r, function(err) {
+            if (err) {
+              return console.log(err);
+            }
+            _logSuccess(getSize(output.length),'POSTCSS BUNDLE SIZE');
+            onDone();
+          });
         }
-      }
-    }
-    let file = bucket[k];
-    fs.readFile(file, 'utf8', cb(file));
+      });
   }
 }
 
+function getSize (bytes) {
+  return bytes < 10000
+    ? bytes.toFixed(0) + ' B'
+    : bytes < 1024000
+    ? (bytes / 1024).toPrecision(3) + ' kB'
+    : (bytes / 1024 / 1024).toPrecision(4) + ' MB'
+}
+
 export default function(options = {}, done) {
-  if(typeof(done) != 'function') done = noOp;
+  if (typeof(done) != 'function') done = noOp;
   const filter = createFilter(options.include, options.exclude);
   const plugins = options.plugins || [];
   const extensions = options.extensions || ['.css', '.sss']
   const output = options.output || './style.css';
   let parse = true;
-  if(options.parse != null){ parse = options.parse }
+  if (options.parse != null) {
+    parse = options.parse
+  }
 
   return {
     ongenerate() {
-      _postcss(plugins, output, done)
+      // No stylesheet needed
+      if (!changes || parse === false) {
+        done();
+        return
+      }
+      changes = 0
+      _postcss(styles, plugins, output, done)
     },
     transform(code, id) {
-      if (!filter(id)) { return null }
-      if (parse) { 
-        if (extensions.indexOf(path.extname(id)) === -1) { return null; }
-        bucket.push(id);
-        return "export default null;"
-      }else{
-        if (extensions.indexOf(path.extname(id)) > -1) { return "export default null;" }
+      if (!filter(id)) {
+        return
+      }
+      if (parse) {
+        if (extensions.indexOf(path.extname(id)) === -1) {
+          return null;
+        }
+        // Keep track of every stylesheet
+        // Check if it changed since last render
+        if (styles[id] !== code && code != '') {
+          styles[id] = code
+          changes++
+        }
+        return ''
+      } else {
+        if (extensions.indexOf(path.extname(id)) > -1) {
+          return ''
+        }
       }
     }
   }
